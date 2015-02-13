48f185a3590b191a9aeda7edae2ccb5ff1ba79a1
Jon Acker <jacker@inviqa.com>
2015-02-13 12:10:21 +0000

    Move mustache to regular deps

--- config.js
+++ config.js
@@ -1,3 +1,17 @@
-define(function () {
+    require.config({
+        baseUrl: 'src',
+        paths: {
+            jquery: 'bower_components/jquery/dist/jquery'
+        }
 
-});
+    });
+
+    //define('mustache', ['../bower_components/mustache/mustache'], function(Mustache) {
+    //    console.log('xxxx');
+    //    return new Mustache();
+    //});
+    //
+    //define(['jquery'], function (jq) {
+    //    return jq.noConflict( true );
+    //});
+
