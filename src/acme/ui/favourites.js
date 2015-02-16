72646def85bef509a22f80c8deca4ab9ecd33800
Jon Acker <jacker@inviqa.com>
2015-02-16 17:30:09 +0000

    Use html template

--- src/acme/ui/favourites.js
+++ src/acme/ui/favourites.js
@@ -1,17 +1,19 @@
 define([
     'jquery',
     'mustache',
-    'acme/lastfm/client'
-], function ($, mustache, lastFmClient) {
+    'acme/lastfm/client',
+    'text!acme/ui/template/favourites.html'
+], function ($, mustache, lastFmClient, template) {
+    var _artists = [{name: 'ABBA'}, {name: 'Bon Jovi'}];
 
     $('#artist-find').click(function() {
         var artistName = $('#artist-name').val();
 
-        lastFmClient.getSimilar(artistName);
+        _render(_artists);
     });
 
-    function _render() {
-
+    function _render(artists) {
+        console.log(mustache.render(template, {artists: _artists}));
     }
 
     return {
